562164d79e93f011325546084fb54b981268e3e0
Sangyun Pae <ceremebsy@gmail.com>
2024-04-02 17:52:55 +0000

    foo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaUnity } from "react-icons/fa";
 import { MdWork } from "react-icons/md";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <p>
+    <main className="flex min-h-screen flex-col items-center p-24 gap-1">
+      <h1>
         cereme (Sangyun Pae)
+      </h1>
+      <hr/>
+      <p>
+        <MdWork className="inline w-5 h-5 pb-1"/>CTO/Producer @ <a href="https://rst.cereme.dev">Bad Sector Games</a>
       </p>
       <p>
-        <MdWork className="inline w-5 h-5"/> Software engineer, Game Producer
+        <FaUnity className="inline"/>
       </p>
       <p>
-        <FaHeart className="inline w-5 h-5 pb-1"/> Hatsune miku, Blue Archive
-      </p>
-      <p>
-        Now in <a href="https://rst.cereme.dev">Bad Sector Games</a>
+        <FaHeart className="inline w-5 h-5 pb-1"/>Hatsune miku, Blue Archive
       </p>
       <p>
         contact:
@@ -23,12 +24,12 @@ export default function Home() {
           ceremebsy@gmail.com
         </a>
       </p>
-      <p>
+      {/* <p>
         TODO: SNS links
       </p>
       <p>
         TODO: blog
-      </p>
+      </p> */}
     </main>
   );
 }
