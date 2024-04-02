ab3f13259d09353881c57bd1799cdc8564aa794d
Sangyun Pae <ceremebsy@gmail.com>
2024-04-02 17:37:05 +0000

    wip

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,9 @@
+import content from './content.json';
+
 export default function Resume() {
     return (
       <main className="flex min-h-screen flex-col items-center p-24">
-
+          {content.headline}
       </main>
     );
   }
