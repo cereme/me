9b4fdc60aa7bf8e2ab7db36c120beb76a7877440
Sangyun Pae <ceremebsy@gmail.com>
2024-04-02 17:25:35 +0000

    foo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { FaHeart } from "react-icons/fa";
+import { MdWork } from "react-icons/md";
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
@@ -5,14 +9,17 @@ export default function Home() {
         cereme (Sangyun Pae)
       </p>
       <p>
-        Software engineer, Game Producer
+        <MdWork className="inline w-5 h-5"/> Software engineer, Game Producer
+      </p>
+      <p>
+        <FaHeart className="inline w-5 h-5 pb-1"/> Hatsune miku, Blue Archive
       </p>
       <p>
         Now in <a href="https://rst.cereme.dev">Bad Sector Games</a>
       </p>
       <p>
         contact:
-        <a href="mailto:ceremebsy@gmail.com">
+        <a href="mailto:ceremebsy@gmail.com" className="ml-2">
           ceremebsy@gmail.com
         </a>
       </p>
